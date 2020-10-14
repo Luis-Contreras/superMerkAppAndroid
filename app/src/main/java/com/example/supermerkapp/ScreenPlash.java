7089d0a4dab2066ef87133c70b8c114ed699b3c4
Davisonby <d.canaveral1@pascualbravo.edu.co>
2020-10-14 14:02:51 -0500

    Screen Plash By Davison

---
+++ app/src/main/java/com/example/supermerkapp/ScreenPlash.java
@@ -0,0 +1,45 @@
+package com.example.supermerkapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ScreenPlash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//Pantalla completa
+        setContentView(R.layout.activity_screen_plash);
+        //animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView tvTitle = findViewById(R.id.tvTitle);
+        TextView tvVersion = findViewById(R.id.tvVersion);
+        ImageView imgLogoMerk = findViewById(R.id.imgLogoMerk);
+
+
+
+        tvTitle.setAnimation(animacion2);
+        tvVersion.setAnimation(animacion2);
+        imgLogoMerk.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ScreenPlash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
